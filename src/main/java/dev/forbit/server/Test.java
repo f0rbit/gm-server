711e71f0004df07a1f9f5e4c5a0378bf7e19004e
f0rbit <dev@forbit.dev>
2021-06-11 21:15:06 +0930

    set up project
    basic hello world program

---
+++ src/main/java/dev/forbit/server/Test.java
@@ -0,0 +1,12 @@
+package dev.forbit.server;
+
+
+import java.util.logging.Logger;
+
+public class Test {
+    public static void main(String[] args) {
+        Logger logger = Logger.getAnonymousLogger();
+        logger.info("hello world");
+    }
+
+}
