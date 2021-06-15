88b2c4ac88adc6ef473da45d930c3df914a934ec
f0rbit <dev@forbit.dev>
2021-06-15 10:35:44 +0930

    update logo

--- example/platformer-basic/server/src/main/java/dev/forbit/platform/Server.java
+++ example/platformer-basic/server/src/main/java/dev/forbit/platform/Server.java
@@ -1,15 +1,12 @@
 package dev.forbit.platform;
 
-
 import dev.forbit.server.Client;
 import dev.forbit.server.ServerInstance;
-import lombok.Getter;
 
 import java.util.Map;
 import java.util.logging.Level;
 
 public class Server extends ServerInstance {
-
 
     public Server(Level logLevel, Map<String, String> variables) {
         super(logLevel, variables);
