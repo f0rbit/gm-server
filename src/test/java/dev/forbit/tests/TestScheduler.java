801d3a8379d957f2f956e60862afd58a8b65a4b3
f0rbit <dev@forbit.dev>
2022-05-03 10:03:36 +0930

    Use Awaitility for timings

--- src/test/java/dev/forbit/tests/TestScheduler.java
+++ src/test/java/dev/forbit/tests/TestScheduler.java
@@ -11,6 +11,9 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("6 - Test Scheduler")
@@ -18,7 +21,7 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestScheduler {
     static final String address = "localhost";
     static final int tcp_port = 28383;
-    static final int udp_port = 38384;
+    static final int udp_port = 28384;
     static Server server;
 
     @BeforeAll
@@ -27,7 +30,7 @@ public class TestScheduler {
         var props = new ServerProperties(address, tcp_port, udp_port);
         server = new GSONServer(props);
         server.init();
-        Thread.sleep(200L);
+        Thread.sleep(300L);
         Utilities.getLogger().info("Finished startup.");
     }
 
@@ -45,7 +48,7 @@ public class TestScheduler {
     @Test
     @Order(2)
     @DisplayName("Test Repeating Task")
-    public void testRepeatingTask() throws InterruptedException {
+    public void testRepeatingTask() {
         final int[] count = {0};
         var task = new RepeatingTask(1, 0, new Runnable() {
             @Override
@@ -55,16 +58,18 @@ public class TestScheduler {
         });
         assertTrue(server.getScheduler().isPresent());
         server.getScheduler().get().addTask(task);
-        Thread.sleep(1500L);
-        assertTrue(count[0] >= 6);
+        /*Thread.sleep(1500L);
+        assertTrue(count[0] >= 6);*/
+        await().atMost(2, SECONDS).until(() -> count[0] >= 6);
         task.stop();
+        assertTrue(true);
     }
 
     @ParameterizedTest
     @Order(3)
     @DisplayName("Test Scheduler timings")
     @ValueSource(ints = {300, 600, 900, 1200})
-    public void testSchedulerTimings(int time) throws InterruptedException {
+    public void testSchedulerTimings(int time) {
         final int[] count = {0};
         var task = new RepeatingTask(1, 0, new Runnable() {
             @Override
@@ -74,21 +79,20 @@ public class TestScheduler {
         });
         assertTrue(server.getScheduler().isPresent());
         server.getScheduler().get().addTask(task);
-        Thread.sleep(time + 300);
-        Utilities.getLogger().info("count: " + count[0] + ", expected: " + time / 200);
-        assertTrue(count[0] >= time / 200);
+        await().atMost(time, MILLISECONDS).until(() -> count[0] >= (time / 200));
         task.stop();
+        assertTrue(true);
     }
 
     @Test
     @Order(4)
     @DisplayName("Test Delayed Task")
-    public void testDelayedTask() throws InterruptedException {
+    public void testDelayedTask() {
         assertTrue(server.getScheduler().isPresent());
         final int[] count = {0};
         var task = new ScheduledTask(4, () -> count[0]++);
         server.getScheduler().get().addTask(task);
-        Thread.sleep(1000L);
+        await().atMost(1500, MILLISECONDS).until(() -> count[0] == 1);
         assertEquals(count[0], 1);
     }
 
